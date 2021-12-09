50e02597922e4af7d2768b573e7c885378db2a91
TCFproject <46992414+ixiondata974@users.noreply.github.com>
2021-12-09 22:34:02 +0100

    Fin du produit et de l'épreuve (à voir pour d'éventuel amélioration)

--- app/src/main/java/fr/epsi/epsiproject/sectionMenu/ProduitDetailActivity.kt
+++ app/src/main/java/fr/epsi/epsiproject/sectionMenu/ProduitDetailActivity.kt
@@ -4,6 +4,11 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
 import fr.epsi.epsiproject.BaseActivity
 import fr.epsi.epsiproject.R
 
@@ -11,6 +16,18 @@ class ProduitDetailActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_produit_detail)
+        val titleSize:TextView = findViewById(R.id.titleHeader)
+        titleSize.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14F)
+
+        val recIntent:Intent = intent
+        showBtnBack()
+        setTitle(recIntent.getStringExtra("nom").toString())
+
+        val imgBigProduit:ImageView = findViewById(R.id.imgUnProduit)
+        val descProduit:TextView = findViewById(R.id.descComplet)
+
+        Picasso.get().load(recIntent.getStringExtra("url")).into(imgBigProduit)
+        descProduit.text = recIntent.getStringExtra("desc")
     }
 
     companion object{
